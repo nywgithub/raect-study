3ce796c5fa7f7d696587df804bcf6efe8d1679c4
niyongwei <1500920628@qq.com>
2021-08-16 16:24:19 +0800

    引入mock.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+//挂载 Mock
+import './mock/index.js'
 import App from './views/App'
 import './index.scss'
 ReactDOM.render(
